8e9ce0fd8683e2998d1ea137d92ed6c1130d0812
Quack1723 <quack1999314@gmail.com>
2021-10-08 01:07:35 +0900

    a

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_lat_lon_from_address(address_l):
     for address in tqdm(address_l):
         payload = {"v": 1.1, 'q': address}
         r = requests.get(url, params=payload)
-        ret = BeautifulSoup(r.content,'lxml')
+        ret = BeautifulSoup(r.content, 'r.parser')
         if ret.find('error'):
             raise ValueError(f"Invalid address submitted. {address}")
         else:
