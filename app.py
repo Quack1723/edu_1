8345eca3fe5a35b6c7c6a0891c6bd4e42fdf9078
Quack1723 <quack1999314@gmail.com>
2021-10-07 23:14:02 +0900

    add requirements.text

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-!pip install folium
-
 import numpy as np
 import random
 import streamlit as st
@@ -8,8 +6,31 @@ import json
 import os
 import time
 import requests
+import tqdm
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
 
-
+def get_lat_lon_from_address(address_l):
+    """
+    address_lにlistの形で住所を入れてあげると、latlonsという入れ子上のリストで緯度経度のリストを返す関数。
+    >>>>get_lat_lon_from_address(['東京都文京区本郷7-3-1','東京都文京区湯島３丁目３０−１'])
+    [['35.712056', '139.762775'], ['35.707771', '139.768205']]
+    """
+    url = 'http://www.geocoding.jp/api/'
+    latlons = []
+    for address in tqdm(address_l):
+        payload = {"v": 1.1, 'q': address}
+        r = requests.get(url, params=payload)
+        ret = BeautifulSoup(r.content,'lxml')
+        if ret.find('error'):
+            raise ValueError(f"Invalid address submitted. {address}")
+        else:
+            lat = ret.find('lat').string
+            lon = ret.find('lng').string
+            latlons.append([lat,lon])
+            time.sleep(10)
+    return latlons
 
 
 def main():
