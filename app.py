7cd762f9bca262ef10089f8fd404710182810017
Quack1723 <quack1999314@gmail.com>
2021-10-08 02:43:08 +0900

    K

--- app.py
+++ app.py
@@ -90,6 +90,7 @@ def make_P_list(Index, dic_lat, W_choice, S, G):
 
     return P, P_W
 
+
 def main():
     # タイトル
     st.title('段ボールEats')
@@ -121,78 +122,6 @@ def main():
         st.dataframe(df)
     json.dump(d, open(cliant_db_path, "w"))
 
-    """
-    # 回収する側
-    st.markdown('** 回収してくれる方はこちらで検索 **')
-    # サンプル用の緯度経度データを作成する
-
-    # ------------------------画面作成------------------------
-
-    st.title("最短経路")  # タイトル
-
-    target = st.slider("運びたい段ボールの個数",
-                       value=10, min_value=10, max_value=40)  # スライダーをつける
-
-    start = '35.6809591,139.7673068'
-    goal = '35.6616778,139.7703389'
-
-    sample_lat = [['35.6786944,139.7745278'],
-                  ['35.6816778,139.7703389'],
-                  ['35.6742222,139.7718056'],
-                  ['35.67425,139.7743333'],
-                  ['35.6715,139.7691389'],
-                  ['35.6781444,139.7693167'],
-                  ['35.6859722,139.7747778'],
-                  ['35.6823069,139.7797439'],
-                  ['35.6841944,139.7784444'],
-                  ['35.6708056,139.7771944']]
-
-    sample_w = []
-    for i in range(len(sample_lat)):
-        sample_w.append(random.randint(1, 20))
-
-    locat = test(sample_lat, sample_w, target, start, goal)
-
-    locations = []
-    for i in locat:
-        locate = i[0].split(',')
-        loc1 = float(locate[0])
-        loc2 = float(locate[1])
-        locations.append([loc1, loc2])
-
-    df = pd.DataFrame({
-        'count': [random.randint(1, 5) for _ in range(len(locations))],
-        'latitude': [lat for (lat, lon) in locations],
-        'longitude': [lon for (lat, lon) in locations]
-    })
-
-    # データを地図に渡す関数を作成する
-    def AreaMarker(df, map):
-        for index, row in df.iterrows():
-            folium.CircleMarker(
-                location=[row['latitude'], row['longitude']],
-                radius=7,
-                color='#0000FF',
-                fill_color='#0000FF'
-            ).add_to(map)
-
-            points = [(a, b) for a, b in zip(df['latitude'].tolist(), df['longitude'].tolist())]
-            folium.PolyLine(points, color="red", weight=2.5, opacity=1).add_to(map)
-
-    # st.subheader("運びたい段ボール{:,}個".format(weight))
-    map = folium.Map(location=[35.6809591, 139.7673068], zoom_start=14)  # 地図の初期設定
-    AreaMarker(df, map)  # データを地図渡す
-    folium.Marker(location=[35.6786944, 139.7745278]).add_to(map)
-    folium.Marker(location=[35.6742222, 139.7718056]).add_to(map)
-    folium.Marker(location=[35.67425, 139.7743333]).add_to(map)
-    folium.Marker(location=[35.6781444, 139.7693167]).add_to(map)
-    folium.Marker(location=[35.6841944, 139.7784444]).add_to(map)
-    folium.Marker(location=[35.6708056, 139.7771944]).add_to(map)
-    folium.Marker(location=[35.6823069, 139.7797439]).add_to(map)
-    folium.Marker(location=[35.6816778, 139.7703389]).add_to(map)
-    folium.Marker(location=[35.6859722, 139.7747778]).add_to(map)
-    folium_static(map)  # 地図情報を表示
-    """
 
 if __name__ == '__main__':
     main()
